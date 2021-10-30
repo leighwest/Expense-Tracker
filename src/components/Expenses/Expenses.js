3975f72e203972be33f6c4e472661d2dbbb35a27
Leigh West <leighwest@hotmail.com>
2021-10-31 09:32:19 +1100

    added toggle for add new expense view

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
-import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 
 const Expenses = props => {
   const [filteredYear, setFilteredYear] = useState('2021')
@@ -9,18 +9,20 @@ const Expenses = props => {
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear);
   }
-  
-  return (   
+
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  })
+
+  return (
     <div>
-      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-      {props.items.map(expense => (
-        <ExpenseItem 
-          key={expense.id}
-          title={expense.title} 
-          amount={expense.amount} 
-          date={expense.date}
-        />
-      ))}
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      
+        <ExpensesList items={filteredExpenses} />
+      
     </div>
   );
 
